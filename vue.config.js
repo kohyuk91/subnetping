916487c27268b208bd83b16c72224733125b9b9f
kohyuk91 <kohyuk91@gmail.com>
2025-01-02 06:43:02 +0900

    add gh-pages package

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  // options...
+  transpileDependencies: true,
+  publicPath: "/subnetping"
+})
